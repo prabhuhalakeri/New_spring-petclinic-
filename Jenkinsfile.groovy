63c6e0dce6211237ac3da83ddd216c677f175380
prabhuH <prabhu.halakeri@gmail.com>
2025-03-13 13:33:59 +0530

    upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,52 @@
+def registry = 'https://trialwf69ge.jfrog.io'
+pipeline{
+    agent any 
+    
+    environment {
+        JAVA_HOME = '/usr/lib/jvm/java-17-amazon-corretto'
+        MAVEN_HOME = '/opt/maven'
+    }
+
+    stages {
+        stage('checkout') {
+            steps {
+               git branch: 'Develop', url: 'https://github.com/sridharbureddy/New-Pet-clinic.git', credentialsId: 'github_cred_01'
+            }
+        }
+
+        stage('build') {
+            steps {
+                script {
+                    sh "${MAVEN_HOME}/bin/mvn clean install"
+                }
+            
+            }
+        }
+     
+        stage("Jar Publish") {
+            steps {
+                script {
+                        echo '<--------------- Jar Publish Started --------------->'
+                        def server = Artifactory.newServer url:registry+"/artifactory" ,  credentialsId:"artifact-cred"
+                        def properties = "buildid=${env.BUILD_ID},commitid=${GIT_COMMIT}";
+                        def uploadSpec = """{
+                            "files": [
+                                {
+                                "pattern": "target/*.jar",
+                                "target": "petclinic-libs-release-local/{1}",
+                                "flat": "false",
+                                "props" : "${properties}",
+                                "exclusions": [ "*.sha1", "*.md5"]
+                                }
+                            ]
+                        }"""
+                        def buildInfo = server.upload(uploadSpec)
+                        buildInfo.env.collect()
+                        server.publishBuildInfo(buildInfo)
+                        echo '<--------------- Jar Publish Ended --------------->'  
+            
+            }
+        }   
+    }      
+    }
+}
